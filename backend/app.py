74f4bd693e68147d30c2ba4bbb53da6ba2fb6afe
Usman Azhar <usman.azhar7100@gmail.com>
2026-04-29 23:47:09 +0500

    backend

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,6 @@ from routes.sessions import sessions_bp
 from routes.orgs     import orgs_bp
 from routes.auth import auth_bp
 from routes.pages import pages_bp
-app.register_blueprint(pages_bp)
 
 app = Flask(__name__, template_folder="templates")
 CORS(app)
@@ -15,6 +14,7 @@ app.register_blueprint(events_bp,   url_prefix="/api/events")
 app.register_blueprint(sessions_bp, url_prefix="/api/sessions")
 app.register_blueprint(orgs_bp,     url_prefix="/api/orgs")
 app.register_blueprint(auth_bp)
+app.register_blueprint(pages_bp)
 
 @app.route("/api/health")
 def health():
